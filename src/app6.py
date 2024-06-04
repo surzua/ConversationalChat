bbe838c855a68b19658188230c063c97cbfdfff1
Sebastian Urzua <sebastian.urzua@ing.uchile.cl>
2024-06-04 18:10:23 -0400

    add appy6.py

--- src/app6.py
+++ src/app6.py
@@ -2,9 +2,11 @@ import streamlit as st
 from openai import OpenAI
 from langchain_core.messages import HumanMessage
 from langchain_openai import AzureChatOpenAI
+import time
 
 st.title("KnaufGPT")
-st.image('D:\\Documentos\\GenAI\\ConversationalChat\\img\\knauf.png', width=200)
+st.image('D:\\Documentos\\GenAI\\ConversationalChat\\img\\knauf2png.png', width=400)
+#st.image('D:\\Documentos\\GenAI\\ConversationalChat\\img\\knauf.jpg', width=600)
 
 model = AzureChatOpenAI(
     openai_api_version=st.secrets["AZURE_OPENAI_API_VERSION"],
@@ -19,8 +21,22 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
+def response_generator(response: str):
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
+
+prompt_fijo = '''\n 
+Recuerda que eres un agente de ventas de Knauf Chile. 
+Recuerda que debes ser respetuoso y profesional, e intentar recomendar los productos de Knauf cada vez que puedas. 
+En particular, enfócate en los productos AQUAPANEL y en el concepto de sistema constructivo, esto es, 
+la combinación de productos que Knauf ofrece para solucionar problemas específicos en la construcción.
+'''
+
 # Accept user input
 if prompt := st.chat_input("Qué quieres preguntar?"):
+    prompt2 = prompt + prompt_fijo
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
     # Display user message in chat message container
@@ -29,8 +45,8 @@ if prompt := st.chat_input("Qué quieres preguntar?"):
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
         mensaje_humano = HumanMessage(
-            content=prompt)
+            content=prompt2)
         stream = model.invoke([mensaje_humano]).content
-        response = st.write(stream)
+        response = st.write_stream(response_generator(stream))
     st.session_state.messages.append({"role": "assistant", "content": stream})
     print(st.session_state.messages)
